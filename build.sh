f744ff5fcdcfbb7eddf9b7ac2a311b6a2034f245
Beau Rogers <Beauerogers@gmail.com>
2015-05-13 15:30:30 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,23 +3,23 @@
 #clean and prepare public directory
 rm -rf public
 # mkdir public
-cp-r src public
+cp -r src public
 #mkdir public
 
 #compile jade to html
 ./node_modules/.bin/jade src -P
 cd src
-find .-name "*.html" | cpio -pdvm ../public
+find . -name "*.html" | cpio -pdvm ../public
 cd ..
 rm -rf src/*/*.html \
        src/**/*.html \
-       public/**/_*.html
+       public/**/_*.html \
        public/_partials
 # compile sass to css
-./node_modules/.bin/node-sass
-  --output-style compressed\
+./node_modules/.bin/node-sass \
+  --output-style compressed \
   --source-map-embed \
-src/_styles/main.scss public/css/main.css
+  src/_styles/main.scss public/css/main.css
 
 #convert ES^ JS to ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
@@ -32,4 +32,4 @@ rm -rf public/_styles \
        public/*.jade \
        public/**/*.jade \
        public/*.scss \
-       public/**/*.scss \
+       public/**/*.scss
