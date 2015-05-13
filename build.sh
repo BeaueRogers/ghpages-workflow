2d9130575ac8349e38c474619a4b23afd26f8cd2
Beau Rogers <Beauerogers@gmail.com>
2015-05-13 11:57:59 -0500

    grrrr

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#clean and prepare public directory
+
+rm -rf public
+mkdir public
+
+#compile jade to html
+./node_modules/.bin/jade src -P
+cd src
+find .-name "*.html" | cpio -pdvm ../public
+cd ..
+rm -rf src/**/*.html
+rm -rf public/**/_*.html
+rm -rf public/_partials
+# compile sass to css
