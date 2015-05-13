bd029142565d2fe00933a0e5273b0e5150f67ea7
Beau Rogers <Beauerogers@gmail.com>
2015-05-13 14:17:50 -0500

    use bowcat to concatenate bower_components during build

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
-#clean and prepare public directory
 
+#clean and prepare public directory
 rm -rf public
-mkdir public
+# mkdir public
+cp-r src public
+#mkdir public
 
 #compile jade to html
 ./node_modules/.bin/jade src -P
 cd src
 find .-name "*.html" | cpio -pdvm ../public
 cd ..
-rm -rf src/**/*.html
-rm -rf public/**/_*.html
-rm -rf public/_partials
+rm -rf src/*/*.html \
+       src/**/*.html \
+       public/**/_*.html
+       public/_partials
 # compile sass to css
+./node_modules/.bin/node-sass
+  --output-style compressed\
+  --source-map-embed \
+src/_styles/main.scss public/css/main.css
+
+#convert ES^ JS to ES5
+./node_modules/.bin/babel src --out-dir public -s inline
+
+#clean uneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss \
